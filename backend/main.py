073e1e87b29d8beae28eafd6a54bc76f8b92753e
freemanlnwza <127272316+freemanlnwza@users.noreply.github.com>
2025-11-07 09:23:39 +0800

    ลองแก้linkให้ตรงกับngrokเลยเผลอผลอลบ main.py

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from database import Base, engine
+from routers import detect, auth, items, search, chats, admin,report
+
+# สร้างตารางถ้ายังไม่มี
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Lost & Found API")
+
+# ========================
+# CORS สำหรับ cookie
+# ========================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://projectlostandfound.netlify.app/","http://localhost:5173",  # frontend domain
+    ],
+    allow_credentials=True,      # สำคัญสำหรับ cookie
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ========================
+# รวม routers
+# ========================
+app.include_router(detect.router)
+app.include_router(auth.router)
+app.include_router(items.router)
+app.include_router(search.router)
+app.include_router(chats.router)
+app.include_router(admin.router)
+app.include_router(report.router)
