e6351274882c43b0c532da8cd2e2d91f9463c9d0
freemanlnwza <127272316+freemanlnwza@users.noreply.github.com>
2025-11-12 11:17:33 +0800

    add api.lostfounditem.com in config

--- frontend/src/configurl.jsx
+++ frontend/src/configurl.jsx
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || "https://projectlostandfounds.netlify.app";
+export const API_URL = import.meta.env.VITE_API_URL || "https://api.lostfounditem.com";
